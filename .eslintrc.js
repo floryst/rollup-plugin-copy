419f4b444f9cf3e154354e3b4e41c5204d64b52e
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-02-18 14:34:50 +0300

    Add sort to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,12 @@ module.exports = {
   rules: {
     'comma-dangle': ['error', 'never'],
     'object-curly-newline': ['error', { multiline: true, consistent: true }],
-    semi: ['error', 'never']
+    semi: ['error', 'never'],
+    'sort-imports': ['error', { ignoreDeclarationSort: true }],
+    'import/order': ['error', { groups: ['builtin', 'external', 'internal'], alphabetize: { order: 'asc' } }],
+    'sort-destructure-keys/sort-destructure-keys': ['error', { caseSensitive: false }]
   },
+  plugins: ['sort-destructure-keys'],
   env: {
     jest: true
   }
